16f1992b612aa9312cbfb757542900b773d12f89
zizo <elgrandesimo53@gmail.com>
2024-11-01 18:52:42 +0100

    sign_in done

--- playground/urls.py
+++ playground/urls.py
@@ -6,11 +6,6 @@ from playground import views as playground_views
 urlpatterns = [
     path('welcome/', views.welcome, name="welcome"),
     path('', views.playground, name="playground"),
-    # path('<str:pk>/', views.playground, name="playground"),
-    path('test/', views.test),
-    # path('sign_in/', views.sign_in, name='sign_in'),
-    # path('sign_up/', views.sign_up, name='sign_up')
-    # path('1/', views.test),
 ]
 
 
