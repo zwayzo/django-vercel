f65f658a13b84be0a3161019be95d3387a73ff6c
zizo <elgrandesimo53@gmail.com>
2024-11-06 17:07:19 +0100

    add items fixed

---
+++ item/forms.py
@@ -0,0 +1,25 @@
+from django import forms 
+
+from .models import Item
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+        }
--- playground/forms.py
+++ playground/forms.py
@@ -1,5 +1,8 @@
 from django import forms 
 from .models import User, Team
+from django.contrib.auth.forms import AuthenticationForm
+
+
 
 class SignUpForm(forms.ModelForm):  # Inherit from forms.ModelForm, not models.Model
     password = forms.CharField(widget=forms.PasswordInput)  # Add widget here for password
